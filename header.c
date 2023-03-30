e38da33959cba0afe4041a552af9192918d6f095
YoshimitsuUnesp <yoshimitsu.lanzo@unesp.br>
2023-03-30 11:32:22 -0300

    Se foder essa merda aq

--- header.c
+++ header.c
@@ -15,26 +15,50 @@ void copy_wav()
     }
 
     // Reads the header
-    fread(&header, sizeof(header), 1, input_file);
+    // fread(&header, sizeof(header), 1, input_file);
 
-    // Needs to decide if will use pointers or change the int types
+    // Collects the header data
+    fseek(input_file, 0, SEEK_SET);
+    int aqui = ftell(input_file);
+    printf("PONTEIRO AQUI 1: %d\n\n", aqui);
+
+    fscanf(input_file, "%s", &header.chunk_id);
+    aqui = ftell(input_file);
+    printf("PONTEIRO AQUI 2: %d\n\n", aqui);
+
+    fscanf(input_file, "%d", &header.chunk_size);
+    aqui = ftell(input_file);
+    printf("PONTEIRO AQUI 2: %d\n\n", aqui);
+
+    // Essa merda dando errado aqui tomar no cu porra caralho se foder buceta do cacete
+    fscanf(input_file, "%s", &header.format);
+    aqui = ftell(input_file);
+    printf("PONTEIRO AQUI 3: %d\n\n", aqui);
+
+    // fscanf(input_file, "%4s %d %4s %4s %d %d", &header.chunk_id, &header.chunk_size, &header.format, &header.subchunk1_id, &header.subchunk1_size, &header.audio_format);
 
     // Prints the collected data
-    printf("File type: %s");
-    printf("File size, excluding header: %d");
-    printf("Subtype: %s");
-    printf("Identifier: %d");
-    printf("Chunck size post header: %d");
-    printf("Format type: %d");
-    printf("Number of channels: %d");
-    printf("Sampling rate: %d");
-    printf("Average number os bits per second: %d");
-    printf("Block alignment in bytes: %d");
-    printf("Resolution: %d");
-    printf("Identifier: %d");
-    printf("Data chunk size: %d");
-    printf("Number of frames to be sampled: %d");
-    printf("Windows length: %d");
+    printf("File type: %s\n", header.chunk_id);
+    printf("File size, excluding header: %d\n", header.chunk_size);
+    printf("Subtype: %s\n");
+    printf("Identifier: %s\n");
+    printf("Chunck size post header: %d\n");
+    printf("Format type: %d\n");
+    printf("Number of channels: %d\n");
+    printf("Sampling rate: %d\n");
+    printf("Average number os bits per second: %d\n");
+    printf("Block alignment in bytes: %d\n");
+    printf("Resolution: %d\n");
+    printf("Identifier: %s\n");
+    printf("Data chunk size: %d\n");
+    printf("Number of frames to be sampled: %d\n");
+    printf("Windows length: %d\n");
+
+    printf("\nPress ENTER to continue\n");
+    getchar();
+    getchar();
+
+    fclose(input_file);
 }
 
 void invert_wav()
--- header.h
+++ header.h
@@ -1,18 +1,18 @@
 typedef struct wav_file
 {
     char chunk_id[4]; // riff
-    int chunk_size;
+    unsigned int chunk_size;
     char format[4]; // wave
-    char subchunk1_id[4];
-    int subchunk1_size;
-    int audio_format;
-    int num_channels;
-    int sample_rate;
-    int byte_rate;
-    int block_align;
-    int bytes_per_sample;
-    char subchunk2_id[4];
-    int subchunk2_size;
+    char subchunk1_id[4]; // fmt
+    unsigned int subchunk1_size;
+    unsigned int audio_format;
+    unsigned int num_channels;
+    unsigned int sample_rate;
+    unsigned int byte_rate;
+    unsigned int block_align;
+    unsigned int bytes_per_sample;
+    char subchunk2_id[4]; // data
+    unsigned int subchunk2_size;
 } Header;
 
 void copy_wav();
