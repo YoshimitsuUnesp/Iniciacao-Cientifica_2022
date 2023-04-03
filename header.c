94105a35c28e03b73338c9b7f734a0b5091af6d6
YoshimitsuUnesp <yoshimitsu.lanzo@unesp.br>
2023-04-03 01:27:38 -0300

    ARQUIVO COPIADO COM SUCESSO

--- header.c
+++ header.c
@@ -2,70 +2,68 @@
 #include <stdlib.h>
 #include "header.h"
 
-void copy_wav()
-{
-    int i;
+void copy_wav(){
     FILE *input_file = fopen("applause.wav", "rb");
     Header header;
 
     // Checks if the file exists
     if (!input_file)
     {
-        printf("ERROR: FILE NOT FOUND");
-        exit(1);
+        printf("\nERROR: FILE NOT FOUND\n\n");
+        return;
     }
 
     // Collects the header data
-    // First chunk
-    fread(&header.chunk_id, sizeof(header.chunk_id) - 1, 1, input_file);
-    fread(&header.chunk_size, sizeof(header.chunk_size), 1, input_file);
-    fread(&header.format,sizeof(header.format) - 1, 1, input_file);
-
-    // Sub-chunk 1
-    fread(&header.subchunk1_id, sizeof(header.subchunk1_id) - 1, 1, input_file);
-    fread(&header.subchunk1_size, sizeof(header.subchunk1_size), 1, input_file);
-    fread(&header.audio_format, sizeof(header.audio_format), 1, input_file);
-    fread(&header.num_channels, sizeof(header.num_channels), 1, input_file);
-    fread(&header.sample_rate, sizeof(header.sample_rate), 1, input_file);
-    fread(&header.byte_rate, sizeof(header.byte_rate), 1, input_file);
-    fread(&header.block_align, sizeof(header.block_align), 1, input_file);
-    fread(&header.bits_per_sample, sizeof(header.bits_per_sample), 1, input_file);
-    
-    // Sub-chunk 2
-    fread(&header.subchunk2_id, sizeof(header.subchunk2_id) - 1, 1, input_file);
-    fread(&header.subchunk2_size, sizeof(header.subchunk2_id), 1, input_file);
-
-    header.chunk_id[4] = '\0';
-    header.format[4] = '\0';
-    header.subchunk1_id[4] = '\0';
-    header.subchunk2_id[4] = '\0';
+    fread(&header, sizeof(header), 1, input_file);
 
     // Prints the collected data
-    printf("File type: %s\n", header.chunk_id);    
+    printf("File type: %c%c%c%c\n", header.chunk_id[0], header.chunk_id[1], header.chunk_id[2], header.chunk_id[3]);    
     printf("File size, excluding header: %d\n", header.chunk_size);
-    printf("Subtype: %s\n", header.format);
-    printf("Identifier: %s\n", header.subchunk1_id);
+    printf("Subtype: %c%c%c%c\n", header.format[0], header.format[1],header.format[2],header.format[3]);
+    printf("Identifier: %c%c%c%c\n", header.subchunk1_id[0],header.subchunk1_id[1],header.subchunk1_id[2],header.subchunk1_id[3]);
     printf("Chunck size post header: %d\n", header.subchunk1_size);
-    printf("Format type: %d\n", header.audio_format); // Erro aqui
-    printf("Number of channels: %d\n", header.num_channels); // Erro aqui
+    printf("Format type: %d\n", header.audio_format); 
+    printf("Number of channels: %d\n", header.num_channels); 
     printf("Sampling rate: %d\n", header.sample_rate);
     printf("Average number os bits per second: %d\n", header.byte_rate);
-    printf("Block alignment in bytes: %hu\n", header.block_align);
-    printf("Resolution: %hu\n", header.bits_per_sample);
-    printf("Identifier: %s\n", header.subchunk2_id);
+    printf("Block alignment in bytes: %d\n", header.block_align);
+    printf("Resolution: %d\n", header.bits_per_sample);
+    printf("Identifier: %c%c%c%c\n", header.subchunk2_id[0],header.subchunk2_id[1],header.subchunk2_id[2],header.subchunk2_id[3]);
     printf("Data chunk size: %d\n", header.subchunk2_size);
     printf("Number of frames to be sampled: %d\n", header.subchunk2_size / header.block_align);
-    printf("Windows length: %d\n", header.subchunk2_size / header.block_align / header.num_channels); // Erro aqui
-
-    short int a;
-    printf("%d", sizeof(a));
+    printf("Windows length: %d\n", header.subchunk2_size / header.block_align / header.num_channels); 
 
     printf("\nPress ENTER to continue\n");
     getchar();
     getchar();
 
-    fclose(input_file);
+    // Copies the file
+    unsigned int num_samples = header.subchunk2_size / (header.bits_per_sample / 8);
+    short int *data = malloc(num_samples * sizeof(short int)); // Allocates the size audio data
+
+    fread(data, sizeof(short int), num_samples, input_file); // Reads the entire data
+
+    fclose(input_file); // Maybe I should comment this
+
+    FILE *output_file = fopen("copy.wav", "wb");
+
+    if(!output_file){
+        printf("\nERROR: COULD NOT OPEN COPY FILE\n\n");
+        return;
+    }
+
+    fwrite(&header, sizeof(header), 1, output_file);
+    fwrite(data, sizeof(short int), num_samples, output_file);
+
+    printf("\nFILE COPIED SUCCESSFULLY!\n\n");
+
+    fclose(output_file);
+
+    free(data);
+
+    return;
 }
+    
 
 void invert_wav()
 {
--- header.h
+++ header.h
@@ -1,22 +1,21 @@
-typedef struct wav_file
-{
-    // It needs 4 characters + \0
-    // int = 4 bytes
-    // unsigned short int = 2 bytes
-    char chunk_id[5]; // riff
-    unsigned int chunk_size;
-    char format[5]; // wave
+typedef struct wav_file{
 
-    char subchunk1_id[5]; // fmt
-    unsigned short int subchunk1_size;
+    // int = 4 bytes
+    // short int = 2 bytes
+    char chunk_id[4]; // riff
+    unsigned int chunk_size;
+    char format[4]; // wave
+
+    char subchunk1_id[4]; // fmt
+    unsigned int subchunk1_size;
     unsigned short int audio_format;
-    unsigned int num_channels;
+    unsigned short int num_channels;
     unsigned int sample_rate;
     unsigned int byte_rate;
     unsigned short int block_align;
     unsigned short int bits_per_sample;
 
-    char subchunk2_id[5]; // data
+    char subchunk2_id[4]; // data
     unsigned int subchunk2_size;
 } Header;
 
